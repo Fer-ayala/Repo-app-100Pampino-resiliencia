57807c00b86ee0cdcd32ae5e7febf58305d8ffa3
Fernando <monasoxx@gmail.com>
2024-10-22 02:52:49 -0500

    iniciando proyecto resiliencia

---
+++ src/main/java/com/example/app_EF_100Pampino_resiliencia/AppEf100PampinoResilienciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_EF_100Pampino_resiliencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEf100PampinoResilienciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEf100PampinoResilienciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app_EF_100Pampino_resiliencia/AppEf100PampinoResilienciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_EF_100Pampino_resiliencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEf100PampinoResilienciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
